d95dfbdaed02ad8c0589e7050d7d78b6e1d501ff
sanhphanvan96 <sanhphanvan96@gmail.com>
2018-03-30 14:46:52 +0700

    Complete simple interact with database(mysql)

--- layout/header.php
+++ layout/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Simple connect database</title>
+	<title>Simple Interact With Database(mysql)</title>
 	<link rel="stylesheet" href="./style.css">
 </head>
 <body>
